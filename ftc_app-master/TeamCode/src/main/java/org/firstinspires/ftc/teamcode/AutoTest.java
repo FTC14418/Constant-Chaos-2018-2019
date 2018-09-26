e684ae0f6ab73b770bf05db8e17ddc8a72a0475b
Nicholas Leone <nicholasjinleone@gmail.com>
2018-09-26 19:43:56 -0400

    9/26/18
    Added while loop for the descent from the elevator.
    
    Added port name for the distance sensor in RobotPreferences.
    
    Started on the encoderDrive method

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,18 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorREV2mDistance;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name="AutoTest", group="Linear Opmode")
 public class AutoTest extends LinearOpMode{
     DcMotor leftMotor;
     DcMotor rightMotor;
+    DistanceSensor distanceSensor1;
     @Override
     public void runOpMode() throws InterruptedException {
         leftMotor = hardwareMap.get(DcMotor.class, RobotPreferences.motor1);
         rightMotor = hardwareMap.get(DcMotor.class, RobotPreferences.motor2);
+        distanceSensor1 = hardwareMap.get(DistanceSensor.class, RobotPreferences.distanceSensor1);
 
-        leftMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        while(distanceSensor1.getDistance(DistanceUnit.INCH)<5.0){
+            leftMotor.setPower(0.5);
+            rightMotor.setPower(-0.5);
+        }
+
+
+
+
+
+    }
+
+    public static void encoderDrive(){
 
     }
 }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotPreferences.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotPreferences.java
@@ -4,5 +4,6 @@ public class RobotPreferences {
 
     public static final String motor1 = "motor1";
     public static final String motor2 = "motor2";
+    public static final String distanceSensor1 = "distanceSensor1";
 
 }
