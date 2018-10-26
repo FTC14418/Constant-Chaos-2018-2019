175d4e2c3cc153cb525e2dd86353a15d244e060a
Nicholas Leone <nicholasjinleone@gmail.com>
2018-10-25 20:21:44 -0400

    Finished Auto1
    - Made basic methods for encoders (setTarget, runtoposition, etc.)
    - Transfered important objects (wheelsize, wheelcircumference, etc.)
    - programmed auto path (calculated distance and robot circumference)

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotPreferences.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotPreferences.java
@@ -8,5 +8,13 @@ public class RobotPreferences {
     public static final String motor4 = "motor4";
     public static final String distanceSensor1 = "distanceSensor1";
     public static final String servo1 = "servo1";
+    public static final Integer encoderCount = 2240;
+    public static final double gearReduction = (25 / 15);
+    public static final double wheelSize = 3.543;
+    public static final double wheelCircumference = wheelSize * Math.PI;
+    public static final double robotDiameter = 11.625;
+    public static final double robotCircumference = robotDiameter*Math.PI;
+    public static final double distancePerPulse = wheelCircumference / (encoderCount * gearReduction);
+    public static final double pulsePerInch = 90;
 
 }
